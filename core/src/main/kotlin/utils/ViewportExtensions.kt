7210d1a60444cc71a218f89801bb4dcc6cdfb0a7
Edgars Upenieks <edgars@vertikali.lv>
2022-12-03 12:27:12 +0200

    Iznesti mainīgie no drawDebug metodes uz globāliem, lai netiktu veidoti, inicializēti un izmesti mainīgie katrā kadrā

--- core/src/main/kotlin/utils/ViewportExtensions.kt
+++ core/src/main/kotlin/utils/ViewportExtensions.kt
@@ -4,41 +4,49 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.utils.viewport.Viewport
 
+var oldColor = Color.BLACK
+var currentWorldWidth: Float = 0f
+var currentWorldHeight: Float = 0f
+var currentXLine: Float = 0f
+var currentYLine: Float = 0f
+
+
 fun Viewport.drawDebugGrid(renderer: ShapeRenderer, cellSize: Int = 1) {
 
 //    copy old renderer color
-    val oldColor = renderer.color.cpy()
+    oldColor = renderer.color.cpy()
 
-//    get world height and with from Viewport class
-    val doubleWorldWidth = worldWidth * 2
-    val doubleWorldHeight = worldHeight * 2
+
+    //    get world height and with from Viewport class
+    currentWorldWidth = worldWidth * 2
+    currentWorldHeight = worldHeight * 2
 
 //    viwport apply gadījumā, ja tiks izmantoti vairāki viewport
-//    apply()
+    apply()
 
     renderer.begin(ShapeRenderer.ShapeType.Line)
     renderer.color = Color.WHITE
 
 //    draw Y axis lines
-    var x: Float = -doubleWorldWidth
-    while (x <= doubleWorldWidth) {
-        renderer.line(x, -doubleWorldHeight, x, doubleWorldHeight)
-        x += cellSize
+    currentXLine = -currentWorldWidth
+    while (currentXLine <= currentWorldWidth) {
+        renderer.line(currentXLine, -currentWorldHeight, currentXLine, currentWorldHeight)
+        currentXLine += cellSize
     }
 
 //    draw X axis lines
-    var y: Float = -doubleWorldHeight
-    while (y <= doubleWorldHeight) {
-        renderer.line(-doubleWorldHeight, y, doubleWorldHeight, y)
-        y += cellSize
+    currentYLine = -currentWorldHeight
+    while (currentYLine <= currentWorldHeight) {
+        renderer.line(-currentWorldHeight, currentYLine, currentWorldHeight, currentYLine)
+        currentYLine += cellSize
     }
 
 //    x axis
     renderer.color = Color.RED
-    renderer.line(-doubleWorldHeight, 0f, doubleWorldWidth, 0f)
+    renderer.line(-currentWorldHeight, 0f, currentWorldWidth, 0f)
 //    y axis
     renderer.color = Color.GREEN
-    renderer.line(0f, -doubleWorldWidth, 0f, doubleWorldWidth)
+    renderer.line(0f, -currentWorldWidth, 0f, currentWorldWidth)
 
 //    draw world bounds
     renderer.color = Color.YELLOW
